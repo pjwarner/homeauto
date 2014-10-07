c7c6818bcc0ed8b69a7354840312ca7d3c7f1075
Paul J Warner <pwarner@raspberrypi.(none)>
2014-10-07 01:56:28 +0000

    Just adding the base files and freezing the pip stuff

--- homeauto/__init__.py
+++ homeauto/__init__.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
 
 from flask import Flask
 app = Flask(__name__)
+
+import pyjade
+import markdown
+from CommonMark import CommonMark
+
+@pyjade.register_filter('markdown')
+@pyjade.register_filter('md')
+def filter_markdown(text, ast):
+    return markdown.markdown(text)
+
+@pyjade.register_filter('commonmark')
+@pyjade.register_filter('cm')
+def filter_commonmark(text, ast):
+    parser = CommonMark.DocParser()
+    renderer = CommonMark.HTMLRenderer()
+    return renderer.render(parser.parse(text))
 
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
